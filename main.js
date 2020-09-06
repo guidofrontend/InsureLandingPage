cfaf48040e30beb069681d6e70294ad2d15cd0d1
Guido Verna Russo <guidofrontend@gmail.com>
2020-09-05 21:01:52 -0300

    falta hacer responsivo el diseño pc

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let menu = document.querySelector('.menu')
 let burguer = document.getElementById('burguer')
+let stylesheet = document.getElementById('stylesheet')
 
 burguer.addEventListener('click', () => menu.classList.toggle('is-active'))
+
+if (window.innerWidth >426) {
+    stylesheet.setAttribute('href', "css/style1.css")
+}
